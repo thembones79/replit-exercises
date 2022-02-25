4be344c47e79e129fd07915b9d780a8116759da3
thembones79 <michal.szumnarski@gmail.com>
2022-02-25 09:58:58 +0100

    done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+console.log("\n✨ ------------  TASK 1  ------------ ✨");
 // 1. Please write a function that shows the usage of closures
 const multiplier = (x) => (y) => x * y;
 const by3 = multiplier(3);
@@ -22,12 +23,14 @@ testTwo(); // will output 11
 testTwo(); // will output 12
 testOne(); // will output 4
 
+console.log("\n✨ ------------  TASK 2  ------------ ✨");
 // 2. Please write a function that returns a sum of array items
 // example input [9, 1, 22, 0, 2]
 // example output 34
 const arrSum = (arr) => arr.reduce((acc, item) => acc + item);
-console.log(arrSum([9, 1, 22, 0, 2])); // will output 3
+console.log(arrSum([9, 1, 22, 0, 2])); // will output 34
 
+console.log("\n✨ ------------  TASK 3  ------------ ✨");
 // 3. Please write a recursive function that flattens a list of items
 // example input [[2, [4, [44,5,6]]], [4,5,6], [[2,4], 4], 5]]
 // example output [2, 4, 44, 5, 6, 4, 5, 6, 2, 4, 4, 5]
@@ -39,13 +42,15 @@ const recursiveFlat = (arr) =>
   );
 console.log(recursiveFlat([[2, [4, [44, 5, 6]]], [4, 5, 6], [[2, 4], 4], 5])); // will output [2, 4, 44, 5, 6, 4, 5, 6, 2, 4, 4, 5]
 
+console.log("\n✨ ------------  TASK 4  ------------ ✨");
 // 4. Please write a function that finds all common elements of two arrays(only primitive types as array elements, order doesn't matter)
 // example inputs ['b', 3, 4, 76, 'c'], ['a', 'b', 4, 76, 21, 'e']
 // example output ['b', 4, 76]
 const commonElements = (arr1, arr2) =>
   arr1.filter((item) => arr2.includes(item));
-console.log(commonElements(["b", 3, 4, 76, "c"], ["a", "b", 4, 76, 21, "e"]));
+console.log(commonElements(["b", 3, 4, 76, "c"], ["a", "b", 4, 76, 21, "e"])); // will output ['b', 4, 76]
 
+console.log("\n✨ ------------  TASK 5  ------------ ✨");
 // 5. Please write a function that finds all different elements of two arrays(only primitive types as array elements, order doesn't matter)
 // example inputs ['b', 3, 4, 76, 'c'], ['a', 'b', 4, 76, 21, 'e']
 // example output ['a', 3, 21, 'c', 'e']
@@ -57,6 +62,7 @@ console.log(
   differentElements(["b", 3, 4, 76, "c"], ["a", "b", 4, 76, 21, "e"])
 ); // will output ['a', 3, 21, 'c', 'e']
 
+console.log("\n✨ ------------  TASK 6  ------------ ✨");
 // 6. Please write a function that takes two arrays of items and returns an array of tuples made from two input arrays at the same indexes. Excessive items should be dropped.
 // example input [1,2,3], [4,5,6,7]
 // example output [[1,4], [2,5], [3,6]]
@@ -70,10 +76,10 @@ const makeTuples = (arr1, arr2) => {
 };
 console.log(makeTuples([1, 2, 3], [4, 5, 6, 7])); // will output [[1,4], [2,5], [3,6]]
 
+console.log("\n✨ ------------  TASK 7  ------------ ✨");
 // 7. Please write a function which takes a path(path is an array of keys) and object, then returns value at this path. If value at path doesn't exists, return undefined.
 // example inputs ['a', 'b', 'c', 'd'], { a: { b: { c: { d: '23' } } } }
 // example output '23'
-
 const pathFinder = (path, obj) => {
   let current = obj;
   for (let i = 0; i < path.length; i++) {
@@ -84,10 +90,28 @@ const pathFinder = (path, obj) => {
 };
 console.log(pathFinder(["a", "b", "c", "d"], { a: { b: { c: { d: "23" } } } })); // will output '23'
 
+console.log("\n✨ ------------  TASK 8  ------------ ✨");
 // 8. Please write compare function which compares 2 objects for equality.
 // example input { a: 'b', c: 'd' }, { c: 'd', a: 'b' }  /// output true
 // example input { a: 'c', c: 'a' }, { c: 'd', a: 'b', q: 's' }  /// output false
+const compareObjects = (obj1, obj2) => {
+  return (
+    Object.entries(obj1).sort().toString() ===
+    Object.entries(obj2).sort().toString()
+  );
+};
+console.log(compareObjects({ a: "b", c: "d" }, { c: "d", a: "b" })); /// will output true
+console.log(compareObjects({ a: "c", c: "a" }, { c: "d", a: "b", q: "s" })); /// will output false
 
+console.log("\n✨ ------------  TASK 9  ------------ ✨");
 // 9. Please write a function which takes a list of keys and an object, then returns this object, just without keys from the list
 // example input ['color', 'size'], { color: 'Blue', id: '22', size: 'xl' }
 // example output { id: '22' }
+function filterObject(arr, obj) {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([key, val]) => !arr.includes(key))
+  );
+}
+console.log(
+  filterObject(["color", "size"], { color: "Blue", id: "22", size: "xl" }) // will output { id: '22' }
+);
